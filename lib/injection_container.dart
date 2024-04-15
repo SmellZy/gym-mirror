647d25d2cb46ab2f5028be80207f094a44fd5027
Dmytro Oleksyuk <ff00d112233@gmail.com>
2024-04-15 16:19:35 +0300

    update DI

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -2,10 +2,14 @@ import 'package:get_it/get_it.dart';
 import 'package:gym_mirror/data/datasources/exercise_local_datasource.dart';
 import 'package:gym_mirror/data/datasources/user_local_datasource.dart';
 import 'package:gym_mirror/data/datasources/workout_local_datasource.dart';
+import 'package:gym_mirror/data/models/exercise/exercise_model.dart';
+import 'package:gym_mirror/data/models/user/user_model.dart';
+import 'package:gym_mirror/data/models/workout/workout_model.dart';
 import 'package:gym_mirror/data/repositories/exercise_repository_impl.dart';
 import 'package:gym_mirror/data/repositories/user_repository_impl.dart';
 import 'package:gym_mirror/data/repositories/workout_repository_impl.dart';
 import 'package:gym_mirror/domain/entities/exercise.dart';
+import 'package:gym_mirror/domain/entities/user.dart';
 import 'package:gym_mirror/domain/repositories/exercise_repository.dart';
 import 'package:gym_mirror/domain/repositories/user_repository.dart';
 import 'package:gym_mirror/domain/repositories/workout_repository.dart';
@@ -25,15 +29,14 @@ import 'package:gym_mirror/domain/usecases/workout/update_workout.dart';
 import 'package:gym_mirror/presentation/bloc/exercise/exercise_bloc.dart';
 import 'package:gym_mirror/presentation/bloc/user/user_bloc.dart';
 import 'package:gym_mirror/presentation/bloc/workout/workout_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
 final getIt = GetIt.instance;
 
-void init() {
-  // Bloc
-  getIt.registerFactory(() => UserBloc(getIt<UserRepository>()));
-  getIt.registerFactory(() => WorkoutBloc(getIt<WorkoutRepository>()));
-  getIt.registerFactory(() => ExerciseBloc(getIt<ExerciseRepository>()));
-
+Future<void> init() async {
+  
   // User Use Cases
   getIt.registerLazySingleton(() => CreateUser(getIt()));
   getIt.registerLazySingleton(() => GetUser(getIt()));
@@ -58,9 +61,20 @@ void init() {
   getIt.registerLazySingleton<WorkoutRepository>(() => WorkoutRepositoryImpl(localDatasource: getIt()));
   getIt.registerLazySingleton<ExerciseRepository>(() => ExerciseRepositoryImpl(localDatasource: getIt()));
 
-
   // Data sources
   getIt.registerLazySingleton<UserLocalDatasource>(() => UserLocalDatasource(userBox: getIt()));
   getIt.registerLazySingleton<WorkoutLocalDatasource>(() => WorkoutLocalDatasource(workoutBox: getIt()));
   getIt.registerLazySingleton<ExerciseLocalDatasource>(() => ExerciseLocalDatasource(exerciseBox: getIt()));
+
+  
+  // Hive
+  getIt.registerLazySingleton<Box<UserModel>>(() => Hive.box<UserModel>('userBox'));
+  getIt.registerLazySingleton<Box<ExerciseModel>>(() => Hive.box<ExerciseModel>('exerciseBox'));
+  getIt.registerLazySingleton<Box<WorkoutModel>>(() => Hive.box<WorkoutModel>('workoutBox'));
+
+
+  // Bloc
+  getIt.registerFactory(() => UserBloc(getIt<UserRepository>()));
+  getIt.registerFactory(() => WorkoutBloc(getIt<WorkoutRepository>()));
+  getIt.registerFactory(() => ExerciseBloc(getIt<ExerciseRepository>()));
 }
